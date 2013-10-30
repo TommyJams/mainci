4396600c28c57f0959c829ecd29ad764418cc0e1
TommyJams <tommyjams.bizspark@outlook.com>
2013-10-30 22:02:44 +0530

    tours 7

--- application/controllers/CFfans.php
+++ application/controllers/CFfans.php
@@ -27,41 +27,4 @@ class CFfans extends CI_Controller{
     $data['campaign'] = json_encode($this->Model->campaignDetails($campaign_id));
 		$this->load->view('campaign_view', $data);
 	}
-
-	public function campaignEvent(){
-
-		require_once('/src/facebook.php');
-
-  		$config = array(
-    				'appId' => '248776888603319',
-    				'secret' => '50f31c2706d846826bead008392e8969',
-  		);
-
-  		$facebook = new Facebook($config);
-  		$user_id = $facebook->getUser();
-
-      $data['user_id'] = $user_id;
-      $data['facebook'] = $facebook;
-      $data['event_id'] = 0;
-
-      $this->load->view('campaignevent_view', $data);     
-	}
-
-	public function postLink(){
-
-		require_once('/src/facebook.php');
-
-  		$config = array(
-    				'appId' => '248776888603319',
-    				'secret' => '50f31c2706d846826bead008392e8969',
-  		);
-
-  		$facebook = new Facebook($config);
-  		$user_id = $facebook->getUser();
-
-  		$data['user_id'] = $user_id;
-  		$data['facebook'] = $facebook;
-
-		  $this->load->view('postlink_view', $data);
-	}
 }
