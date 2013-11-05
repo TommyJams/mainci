4f26eee13869dfd21329d650c9244a53a4345e54
TommyJams <tommyjams.bizspark@outlook.com>
2013-11-05 14:34:00 +0530

    contributors css

--- application/views/campaign_view.php
+++ application/views/campaign_view.php
@@ -296,10 +296,12 @@
                     $fans_contact = $contributor->contact;
                     $fans_location = $contributor->location;
                   ?>
-                  <h4><? print($fans_name); ?></h4>
-                  <h5>INR <? print($fans_contribution); ?></h5>
-                  <h5><? print($fans_contact); ?></h5>
-                  <h5><? print($fans_location); ?></h5>
+                  <div class="col-md-3">
+                    <h4><? print($fans_name); ?></h4>
+                    <h5>INR <? print($fans_contribution); ?></h5>
+                    <h5><? print($fans_contact); ?></h5>
+                    <h5><? print($fans_location); ?></h5>
+                  </div>  
                   <div class="seperator" ></div>
                   <? 
                       }
