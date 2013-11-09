28d0b4adb6df11dfbd6eb0b9837a462791d4d397
TommyJams <tommyjams.bizspark@outlook.com>
2013-11-09 19:00:11 +0530

    Revert "camp view change 16"
    This reverts commit 1b789a0170426c87c468297d1e53a40dac3852bc.

--- application/views/campaign_view.php
+++ application/views/campaign_view.php
@@ -222,7 +222,7 @@
             </ul>
             <div class="clearfix"></div>
             <?
-            if($fbEventURL)
+            if($fbEventURL && $fbEventName)
             {
             ?>
               <div class="seperator" ></div>
