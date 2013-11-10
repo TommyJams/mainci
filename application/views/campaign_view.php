81b630d2b7aeff30959e870fda7974df96564cc8
TommyJams <tommyjams.bizspark@outlook.com>
2013-11-10 20:52:49 +0530

    camp view change 7

--- application/views/campaign_view.php
+++ application/views/campaign_view.php
@@ -116,10 +116,10 @@
         <div class="col-sm-12 col-md-5 d-tj-black-box-container" >
           <div class="d-tj-black-box d-tj-tour-right" > 
             <h3 style="margin-top: 5px; margin-bottom: 20px; text-transform:none;"><? print($artist_name); ?></h3>
-            <h2 class="raise" style="font-size:40px">Rs <? print($raised); ?></h2> funded of Rs <? print($target); ?>    
-            <h2 class="tgt" style="font-size:40px"><? print($totalPledges); ?></h2> contributors
+            <h2 class="raise" style="font-size:40px">Rs <? print($raised); ?> <p>funded of Rs <? print($target); ?></p></h2>     
+            <h2 class="tgt" style="font-size:40px"><? print($totalPledges); ?> contributors</h2> 
             <!--<h4 class="tgt" >TARGET : INR <? //print($target); ?></h4>-->
-            <h2 style="font-size:40px"><? print($days_to_go); ?></h2> days to go
+            <h2 style="font-size:40px"><? print($days_to_go); ?> days to go</h2> 
             <!--<h4 style="line-height:0.5px"> days to go </h4>-->
             <div class="text-center d-tj-offset-top-40 pledge-btn-top">
               <input type="button" value="BUY NOW" onclick="window.open('<? print($ticket_widget); ?>', '_blank');" >
