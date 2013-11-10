2d3a6f421af69f46177413172aa603ed3218b6f3
TommyJams <tommyjams.bizspark@outlook.com>
2013-11-10 21:16:20 +0530

    rs symbol 1

--- application/views/campaignedit_view.php
+++ application/views/campaignedit_view.php
@@ -99,7 +99,7 @@
           <table class="table table-hover" >
             <thead>
               <tr >
-                <th>PLEDGE AMOUNT (INR.)</th>
+                <th>PLEDGE AMOUNT (&#8377)</th>
                 <th>BENEFIT</th>
               </tr>
             </thead>
@@ -207,7 +207,7 @@
           </div>
           <div class="col-sm-12 col-md-5 d-tj-black-box-container" >
             <div class="d-tj-black-box d-tj-tour-right-edit" >
-              <input class="form-control input-lg pull-left" type="text" id="target" name="target" placeholder="ENTER TARGET AMOUNT [MIN: Rs <? print($min_target); ?>]"></input>
+              <input class="form-control input-lg pull-left" type="text" id="target" name="target" placeholder="ENTER TARGET AMOUNT [MIN: &#8377 <? print($min_target); ?>]"></input>
               <input class="form-control input-lg pull-left" type="text" id="phone" name="phone" placeholder="ENTER PHONE NUMBER [10-DIGIT]"></input>
               <input class="form-control input-lg pull-left" type="text" id="email" name="email" placeholder="ENTER EMAIL ADDRESS"></input>
               <div class="clearfix"></div>
