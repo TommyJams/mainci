f801645ba0987f3c1b47cde4fd3d4f77ed9916fa
TommyJams <tommyjams.bizspark@outlook.com>
2013-11-10 16:43:28 +0530

    page title changes

--- application/views/campaignedit_view.php
+++ application/views/campaignedit_view.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>TommyJams - StartCampaign</title>
+<title>TommyJams - Start Campaign</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="favicon.ico" rel="shortcut icon">
 <!-- Bootstrap -->
