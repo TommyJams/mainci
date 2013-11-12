6419c41dc8f218d89f5ce60c0cbe5a7891d0d2fd
TommyJams <tommyjams.bizspark@outlook.com>
2013-11-12 15:33:49 +0530

    vid 11

--- application/views/landingpage_view.php
+++ application/views/landingpage_view.php
@@ -483,21 +483,20 @@
 
   $("#watchdemo").click(function() {
     $.fancybox($('.watchdemo').html(),{
-        'padding'   : 0,
-        'autoScale'   : false,
+        'padding'       : 0,
+        'autoScale'     : false,
         'transitionIn'  : 'none',
         'transitionOut' : 'none',
-        'title'     : this.title,
-        'width'   : 680,
-        'height'    : 495,
-        'href'      : href.replace(new RegExp("watch\\?v=BRQpt34-ocE", "i"), 'v/'),
-        'type'      : 'swf',
+        'title'         : this.title,
+        'width'         : 680,
+        'height'        : 495,
+        'href'          : this.href.replace(new RegExp("watch\\?v=BRQpt34-ocE", "i"), 'v/'),
+        'type'          : 'swf',
         'swf'     : {
-             'wmode'    : 'transparent',
-          'allowfullscreen' : 'true'
-        }
-      });
-
+                      'wmode'    : 'transparent',
+                      'allowfullscreen' : 'true'
+                    }
+    });
     return false;
   });
 
