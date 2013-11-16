5fd38e7272f259f75bb8a28cff27a8c1c96cb022
TommyJams <tommyjams.bizspark@outlook.com>
2013-11-16 14:08:11 +0530

    roadshows vid add 1

--- application/views/landingpage_view.php
+++ application/views/landingpage_view.php
@@ -219,7 +219,7 @@
           src="http://www.youtube.com/embed/kcOo3ge5URE" frameborder="0" allowfullscreen></iframe>
         </div>  
         <div class="col-sm-12 col-md-5 d-tj-black-box-container" >
-          <h2>TOMMYJAMS ROADSHOWS</h2>
+          <h2>TOMMYJAMS ROADSHOWS </h2>
           <div class="who-campaigns">
             <img align="left" src="/img/roadshows_logo.png" style="margin-right:5px;width:120px">
             <h5>The RoadShows&#39 campaigns are initiated by the artists for the tours that they want to venture on. Every time you buy a ticket, an artist gets one step closer to his dream tour across various cities in the country. Not only that, you get exclusive access to all the shows which are part of that tour, and even get extra freebies from the band on-the-day to make the event really special for you.
