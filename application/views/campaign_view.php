a2d7b8ef7fc31eed8235bcbac2570aca1646c4a9
TommyJams <tommyjams.bizspark@outlook.com>
2013-11-17 16:08:56 +0530

    meta fields change 1

--- application/views/campaign_view.php
+++ application/views/campaign_view.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml"  xmlns:og="http://ogp.me/ns#" xmlns:fb="http://www.facebook.com/2008/fbml" >
+<html xmlns:fb="http://www.facebook.com/2008/fbml" xmlns:og="http://opengraphprotocol.org/schema/" xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr" >
 <head>
 <title>TommyJams - Apply Now</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
