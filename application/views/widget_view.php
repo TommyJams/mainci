1d84489d9e9c7801f242e832ec7bbdf94d90aba2
TommyJams <tommyjams.bizspark@outlook.com>
2013-11-17 15:28:23 +0530

    ms ventures url add

---
+++ application/views/widget_view.php
@@ -0,0 +1,358 @@
+<html>
+<head>
+</head>
+<body style="margin:0; padding:0;">
+
+<div class="container " id="ticketDiv">
+	<div class="row">
+        <div class="span12 rightside_pannel">
+        	            <div class="row-fluid">
+				<div class="span12">
+					<div class="gray_pannel" id="ticketEngine">
+<!---->
+
+<form name="frmTicketInfo" id="frmTicketInfo" method="post" enctype="application/x-www-form-urlencoded" action="https://em.explara.com/ticket/process/order" target="_blank">
+
+<div class="row-fluid">
+    <div class="span12">
+    	<div class="blue_box"><span>SELECT YOUR TICKET</span></div>
+    </div>
+</div>
+<div class="gray_pannel">
+<div class="row-fluid">
+    <div class="span8 mob_span8">
+	
+    	<h5>Bronze Ticket</h5>
+		<p><small>Entry to both gigs (tickets transferable)<br>		<em>Till 11:59&nbsp;PM,&nbsp;08&nbsp;Dec&nbsp;2013</em></small></p>
+    </div>
+    
+    <div class="span4 mob_span4">
+    	<div class="select_ticketno select_style">
+		
+        						<select id="11479_value_qty" onchange="showCartValue('11479');" name="quantity[]">
+				<option value="0"></option>
+																												<option value="1">1</option>
+																									<option value="2">2</option>
+																									<option value="3">3</option>
+																									<option value="4">4</option>
+																									<option value="5">5</option>
+																									<option value="6">6</option>
+																									<option value="7">7</option>
+																									<option value="8">8</option>
+																									<option value="9">9</option>
+																									<option value="10">10</option>
+																								</select>
+			                </div>
+    </div>
+</div>
+<div class="row-fluid">
+    <div class="span8 mob_span8">
+    	<h5>					<img src="https://assets.explara.com/public/v_5.4.4/images/icon_inr.png" width="10" height="10"> 	200
+						</h5>
+    </div>
+    <input type="hidden" name="ticketId[]" value="11479">
+    <div class="span4 mob_span4">
+    	<h4><span id="11479_currency" style="display:none;"><img src="https://assets.explara.com/public/v_5.4.4/images/icon_inr.png" width="10" height="10"></span> <span class="finalPrice" id="finalPrice_11479"></span></h4>
+    </div>
+</div>
+
+
+ 
+<hr style="margin:8px 0px; clear:both;">
+
+
+<div class="row-fluid">
+    <div class="span8 mob_span8">
+	
+    	<h5>Silver Ticket</h5>
+		<p><small>Entry to both gigs + Band artwork + Digital download of Gig &amp; Studio recordings<br>		<em>Till 11:59&nbsp;PM,&nbsp;08&nbsp;Dec&nbsp;2013</em></small></p>
+    </div>
+    
+    <div class="span4 mob_span4">
+    	<div class="select_ticketno select_style">
+		
+        						<select id="11480_value_qty" onchange="showCartValue('11480');" name="quantity[]">
+				<option value="0"></option>
+																												<option value="1">1</option>
+																									<option value="2">2</option>
+																									<option value="3">3</option>
+																									<option value="4">4</option>
+																									<option value="5">5</option>
+																									<option value="6">6</option>
+																									<option value="7">7</option>
+																									<option value="8">8</option>
+																									<option value="9">9</option>
+																									<option value="10">10</option>
+																								</select>
+			                </div>
+    </div>
+</div>
+<div class="row-fluid">
+    <div class="span8 mob_span8">
+    	<h5>					<img src="https://assets.explara.com/public/v_5.4.4/images/icon_inr.png" width="10" height="10"> 	400
+						</h5>
+    </div>
+    <input type="hidden" name="ticketId[]" value="11480">
+    <div class="span4 mob_span4">
+    	<h4><span id="11480_currency" style="display:none;"><img src="https://assets.explara.com/public/v_5.4.4/images/icon_inr.png" width="10" height="10"></span> <span class="finalPrice" id="finalPrice_11480"></span></h4>
+    </div>
+</div>
+
+
+ 
+<hr style="margin:8px 0px; clear:both;">
+
+
+<div class="row-fluid">
+    <div class="span8 mob_span8">
+	
+    	<h5>Gold Ticket</h5>
+		<p><small>Entry to both gigs + Band artwork + Digital download of Gig &amp; Studio recordings + Chill/Jam with band<br>		<em>Till 11:59&nbsp;PM,&nbsp;08&nbsp;Dec&nbsp;2013</em></small></p>
+    </div>
+    
+    <div class="span4 mob_span4">
+    	<div class="select_ticketno select_style">
+		
+        						<select id="11481_value_qty" onchange="showCartValue('11481');" name="quantity[]">
+				<option value="0"></option>
+																												<option value="1">1</option>
+																									<option value="2">2</option>
+																									<option value="3">3</option>
+																									<option value="4">4</option>
+																									<option value="5">5</option>
+																									<option value="6">6</option>
+																									<option value="7">7</option>
+																									<option value="8">8</option>
+																									<option value="9">9</option>
+																									<option value="10">10</option>
+																								</select>
+			                </div>
+    </div>
+</div>
+<div class="row-fluid">
+    <div class="span8 mob_span8">
+    	<h5>					<img src="https://assets.explara.com/public/v_5.4.4/images/icon_inr.png" width="10" height="10"> 	800
+						</h5>
+    </div>
+    <input type="hidden" name="ticketId[]" value="11481">
+    <div class="span4 mob_span4">
+    	<h4><span id="11481_currency" style="display:none;"><img src="https://assets.explara.com/public/v_5.4.4/images/icon_inr.png" width="10" height="10"></span> <span class="finalPrice" id="finalPrice_11481"></span></h4>
+    </div>
+</div>
+
+
+ 
+<hr style="margin:8px 0px; clear:both;">
+
+
+<div class="row-fluid visible-phone">
+    <div class="span4 mob_span4">
+    	<h5>TOTAL</h5>
+    </div>
+    <div class="span8 mob_span8">
+    	<h4>
+        	<img src="https://assets.explara.com/public/v_5.4.4/images/icon_inr.png" width="10" height="10">
+			  <span class="finalPriceMainTotal">0</span>
+        </h4>
+    </div>
+</div>
+<hr class="visible-phone" style="margin:8px 0px; clear:both;">
+<div class="row-fluid visible-phone">
+    <div class="span4 mob_span4">
+        <p><small>(-) Discount</small></p>
+    </div>
+    <div class="span8 mob_span8">
+        <h4 style="margin-top:0px;"><img src="https://assets.explara.com/public/v_5.4.4/images/icon_inr.png" width="10" height="10">
+			  <span class="finalPriceDiscount">0</span></h4>
+    </div>
+</div>
+<div class="row-fluid visible-phone">
+    <div class="span4 mob_span4">
+        <p><small>(+) Processing Fee</small></p>
+    </div>
+    <div class="span8 mob_span8">
+        <h4 style="margin-top:0px;"><img src="https://assets.explara.com/public/v_5.4.4/images/icon_inr.png" width="10" height="10">
+			  <span class="finalPriceMainProcessing">0</span></h4>
+    </div>
+</div>
+<div class="row-fluid visible-phone">
+    <div class="span4 mob_span4">
+        <p><small>(+) Service Tax</small></p>
+    </div>
+    <div class="span8 mob_span8">
+        <h4 style="margin-top:0px;"><img src="https://assets.explara.com/public/v_5.4.4/images/icon_inr.png" width="10" height="10">
+			  <span class="finalServiceFee">0</span></h4>
+    </div>
+</div>
+<div class="row-fluid hidden-phone">
+    <div class="span4 mob_span4">
+    	<h5>TOTAL</h5>
+    </div>
+    <div class="span8 mob_span8">
+    	<h4>
+        	<img src="https://assets.explara.com/public/v_5.4.4/images/icon_inr.png" width="10" height="10">
+			  <span class="finalPriceMainTotal">0</span>
+        </h4>
+    </div>
+</div>
+<div class="row-fluid hidden-phone">
+    <div class="span4 mob_span4">
+        <p><small>(-) Discount</small></p>
+    </div>
+    <div class="span8 mob_span8">
+        <h4 style="margin-top:0px;"><img src="https://assets.explara.com/public/v_5.4.4/images/icon_inr.png" width="10" height="10">
+			  <span class="finalPriceDiscount">0</span></h4>
+    </div>
+</div>
+<div class="row-fluid hidden-phone">
+    <div class="span4 mob_span4">
+        <p><small>(+) Processing Fee</small></p>
+    </div>
+    <div class="span8 mob_span8">
+        <h4 style="margin-top:0px;"><img src="https://assets.explara.com/public/v_5.4.4/images/icon_inr.png" width="10" height="10">
+			  <span class="finalPriceMainProcessing">0</span></h4>
+    </div>
+</div>
+<div class="row-fluid hidden-phone">
+    <div class="span4 mob_span4">
+        <p><small>(+) Service Tax</small></p>
+    </div>
+    <div class="span8 mob_span8">
+        <h4 style="margin-top:0px;"><img src="https://assets.explara.com/public/v_5.4.4/images/icon_inr.png" width="10" height="10">
+			  <span class="finalServiceFee">0</span></h4>
+    </div>
+</div>
+<hr style="height:2px; color:#333; background:#333; margin:8px 0px;">
+<div class="row-fluid">
+    <div class="span4 mob_span4">
+    	<h5 style="padding-top:10px;">TO PAY</h5>
+    </div>
+    <div class="span8 mob_span8">
+    	<h2 style="padding-right:10px; float:right;">
+        	<img src="https://assets.explara.com/public/v_5.4.4/images/icon_inr.png" width="10" height="10">
+				  <span class="finalGrandTotal">0</span>
+        </h2>
+    </div>
+</div>
+<hr style="height:2px; color:#333; background:#333; margin:8px 0px;">
+
+<div class="row-fluid">
+    <div class="span12">
+    	<div class="button_blue16 fl" style="padding:10px; font-size:16px;">
+            <a href="javascript:void(0);" onclick="$('#frmTicketInfo').submit();"><span style="width:100px; padding-left:10px;"><strong>Checkout</strong></span> <img src="https://assets.explara.com/public/v_5.4.4/images/icon_checkout.png" width="25" height="25"></a>
+			<br>
+	   		<p class="fl" style="text-transform:none; color:#333; font-weight:normal;"><small>Pay by Cash / Net Banking /<br>
+	            Debit or Credit Cards or COD</small></p>
+		</div>
+				<div class=" fr" style="padding:10px; font-size:16px;"><a href="http://www.explara.com/" target="_blank" class="fl"><img src="https://assets.explara.com/public/v_5.4.4/images/logo_poweredby.png" width="" height="" style="float:left; padding-bottom:10px; padding-right:10px;"></a></div>
+   		    </div>
+</div>
+
+<input type="hidden" name="eventId" id="eventId" value="9176">
+<input type="hidden" name="hasJavascript" id="hasJavascript" value="1">
+
+</div></form></div>
+				</div>
+            </div>
+                    </div>
+	</div>
+</div>
+
+<div class="clb"></div>
+
+<script type="text/javascript" src="https://em.explara.com/public/v_5.4.4/js/jquery/jquery-1.8.3.min.js"></script>
+<script type="text/javascript">
+getTickets();
+function viewHeight(){
+	var upPos   = document.getElementById('ticketDiv').offsetHeight;
+	if((parent.document.getElementById('ifrm')) && (parent.document.getElementById('frameData')) ){
+		upPos	= upPos+80;
+		parent.document.getElementById('ifrm').style.height = upPos+'px';
+		//parent.document.getElementById('ifrm').style.width = 300+'px';
+		parent.document.getElementById('frameData').value 	    = '<iframe frameborder="0" src="http://em.explara.com/widget/tommyjams-thekanchandanielband" width="300" height="'+upPos+'"></iframe>';
+	}
+}
+function getTickets(){
+	var dataString = 'eventId=9176&target=new';
+	var response = $.ajax({
+	   type: "POST",
+	   data: dataString,
+	   url: "/event/index/get-tickets",
+	   success: function(response){
+			$('#ticketEngine').html(response);
+			viewHeight();			
+	   }
+	});	
+}
+function flipOption(classId) {
+	$('#ticketMainView').html('');
+	$('#progressBarOrg').show();
+	var reqData = 'eventId=9176&categoryId='+classId+'&target=new';
+	var response = $.ajax({
+	   type: "POST",
+	   url: "/event/index/get-ticket-type",
+	   data: reqData,
+	   success: function(response){
+			$('#ticketMainView').html(response);	
+			$('#ticketMainView').show();
+		}
+    }); 
+}
+var total			= 0;
+function showCartValue(ticketId) {
+	var qtySelected	= $('#'+ticketId+'_value_qty').val();
+	var reqData 	= $('#frmTicketInfo').serialize();
+	var response 	= $.ajax({
+	   type: "POST",
+	   dataType: "json",
+	   url: "/event/index/generate-cart",
+	   data: reqData,
+	   success: function(response){
+	   		if(response.carts.codeError == true) {
+				$('#messageDivModal').html('Your entered discount code is not valid');
+				$("#messageModal").modal({
+					close:true
+				});	
+			}
+			$('.finalPrice').html('');
+			$('#mainPriceTotal').hide();
+			$('#mainPriceTotalBlank').show();
+			$.each(response.carts.cart, function(entryIndex, entry) {
+				if(entry.ticketTypeId) {
+					if(entry.rowPrice == 0.00) {
+						$('#finalPrice_'+entry.ticketTypeId).html(0);	
+						$('#'+entry.ticketTypeId+'_currency').show();
+					}else {
+						$('#finalPrice_'+entry.ticketTypeId).html(entry.rowPrice);
+						$('#'+entry.ticketTypeId+'_currency').show();
+					}
+				}
+			});
+			if(response.carts.conversion != '0.00') {
+				$('#converted').html('Note: 1 USD = '+response.carts.conversion+' INR');	
+			}
+			if(response.carts.total == '0.00') {
+				$('.finalPriceMainTotal').html(response.carts.actualCost);
+				$('.finalPriceMainProcessing').html(response.carts.servieFeeTotal);
+				$('.finalPriceDiscount').html(response.carts.totalDiscount);
+				$('.finalGrandTotal').html(response.carts.grandTotal);	
+				$('.finalServiceFee').html(response.carts.serviceTax);
+			}else {
+				total		= response.carts.total;
+				$('.finalPriceMainTotal').html(response.carts.actualCost);
+				$('.finalPriceMainProcessing').html(response.carts.servieFeeTotal);
+				$('.finalPriceDiscount').html(response.carts.totalDiscount);
+				$('.finalGrandTotal').html(response.carts.grandTotal);
+				$('.finalServiceFee').html(response.carts.serviceTax);
+			}
+		}
+   });
+}
+</script>
+
+
+
+
+
+</body>
+</html>
