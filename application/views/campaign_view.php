6aa66845e89047793303abaa98c8106a6ec5d05c
TommyJams <tommyjams.bizspark@outlook.com>
2013-11-17 18:41:34 +0530

    fb share change 3

--- application/views/campaign_view.php
+++ application/views/campaign_view.php
@@ -83,11 +83,13 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>TommyJams - <?print($artist_name);?></title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta property="og:title" content="<?print($artist_name);?>" />
 <meta property="og:image" content="<? print(base_url().'images/artist/campaign/'.$backimg); ?>"/>
 <meta property="og:description" content="<?print($artist_name);?> is touring with TommyJams and coming to a venue near you. Pre-book your tickets now! \nTarget Sales: <? print($target); ?>" />
+
+<title>TommyJams - <?print($artist_name);?></title>
 
 <link href="favicon.ico" rel="shortcut icon">
 <!-- Bootstrap -->
