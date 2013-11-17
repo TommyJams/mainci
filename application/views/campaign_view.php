25ae15fddb4156d2925d1c7f95c1730969a357ab
TommyJams <tommyjams.bizspark@outlook.com>
2013-11-17 18:08:58 +0530

    meta fields change 17

--- application/views/campaign_view.php
+++ application/views/campaign_view.php
@@ -1,3 +1,8 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <!--venue modal-->
 <? $tourDetail = (json_decode($campaign));
   foreach($tourDetail as $tourDetail) 
@@ -80,11 +85,6 @@
   }
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta property="og:title" content="<?print($artist_name);?>" />
 <meta property="og:image" content="<? print(base_url().'images/artist/campaign/'.$backimg); ?>"/>
 <meta property="og:url" content="http://testtommyjams.azurewebsites.net/campaign/<?print($campaign_id);?>" />
