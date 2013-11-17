c05ee27c6389a961140885b47e54c8e3e2a6763b
TommyJams <tommyjams.bizspark@outlook.com>
2013-11-17 18:35:43 +0530

    fb share change 1

--- application/views/campaign_view.php
+++ application/views/campaign_view.php
@@ -1,6 +1,4 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-
 <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!--[if lt IE 9]>
       <script src="js/html5shiv.js"></script>
@@ -83,6 +81,8 @@
     $fbLoginURL = $row->fbLoginURL;
   }
 ?>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <title>TommyJams - Apply Now</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
