b7f144e4d3df53b5814bc5a79c01f6709f0c4034
TommyJams <tommyjams.bizspark@outlook.com>
2013-11-17 16:45:49 +0530

    meta fields change 4

--- application/views/campaign_view.php
+++ application/views/campaign_view.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
 <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!--[if lt IE 9]>
       <script src="js/html5shiv.js"></script>
@@ -87,7 +86,7 @@
 
   }
 ?>
-
+<head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta property="og:title" content="<?print($artist_name);?>" >
