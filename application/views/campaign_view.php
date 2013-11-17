ce60b5dc4236859ef1c5e3fc3876b5394b461a56
TommyJams <tommyjams.bizspark@outlook.com>
2013-11-17 19:56:04 +0530

    fb share change 8

--- application/views/campaign_view.php
+++ application/views/campaign_view.php
@@ -15,6 +15,8 @@
     $link = $venue->link;
     $contact = $venue->contact;
   ?>
+    <html>
+    <body>
     <div class="venue-form<? print($venue_id); ?>" style="display: none;" >
       <div class="modal-content socialModal">
         <div class="modal-header">
@@ -41,7 +43,9 @@
           <a href="javascript:;" onclick="$.fancybox.close();" class="btn blk-btn" data-dismiss="modal">Close</a> 
         </div>
       </div>
-    </div>  
+    </div>
+  </body>
+  </html>
 <? } ?>
 <!--/venue modal-->    
 
