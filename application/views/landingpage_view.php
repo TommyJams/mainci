813032b383c3a9b02069463bc43b7c684fb79746
TommyJams <tommyjams.bizspark@outlook.com>
2013-11-19 12:34:39 +0530

    overlay image fix 6

--- application/views/landingpage_view.php
+++ application/views/landingpage_view.php
@@ -213,10 +213,7 @@
                 <h4 class="d-tj-slide-head" ><? print($artist_name); ?></h4>
                 <div class="d-tj-slide-body " style="">
                   <div class="d-tj-slide-img" onclick="window.open('<?print(base_url().'campaign/'.$campaign_id);?>', '_blank');" style="background-image:url(<? print(base_url().'images/artist/campaign/'.$image); ?>)">  
-                    <div class="d-tj-slide-hover-img">
-                    <a class="d-tj-slide-hover-img">  
-                      <span class="d-tj-slide-image-overlay" align="left"> </span>
-                    </a>  
+                    <div class="d-tj-slide-hover-img">  
                     </div>
                   </div>
                   <div class="d-tj-progress">
