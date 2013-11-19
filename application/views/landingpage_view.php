5e23a779de5a47ff3ee1716b6365581957eca001
TommyJams <tommyjams.bizspark@outlook.com>
2013-11-19 13:26:16 +0530

    overlay image fix 13

--- application/views/landingpage_view.php
+++ application/views/landingpage_view.php
@@ -213,8 +213,7 @@
                 <h4 class="d-tj-slide-head" ><? print($artist_name); ?></h4>
                 <div class="d-tj-slide-body " style="">
                   <div class="d-tj-slide-img" onclick="window.open('<?print(base_url().'campaign/'.$campaign_id);?>', '_blank');" style="background-image:url(<? print(base_url().'images/artist/campaign/'.$image); ?>)">  
-                    <div class="d-tj-slide-overlay-img"> <img src="/image/radioone/icon/image_overlay_grey.png" alt="" />   
-                    </div>
+                    <div class="d-tj-slide-overlay-img"> </div>
                   </div>
                   <div class="d-tj-progress">
                     <div class="d-tj-progress-g" style="width:<? print($funded); ?>%;"> </div>
