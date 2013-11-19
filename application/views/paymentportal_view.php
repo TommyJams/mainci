40746cafc4111c4c5c6a5f70066d6c9b9121d292
TommyJams <tommyjams.bizspark@outlook.com>
2013-11-19 15:52:03 +0530

    payment portal change 1

--- application/views/paymentportal_view.php
+++ application/views/paymentportal_view.php
@@ -9,16 +9,63 @@
 <link href="/stylecf/bootstrap.min.css" rel="stylesheet" media="screen">
 <link href="/stylecf/tj.css" rel="stylesheet" media="screen">
 <link href="http://netdna.bootstrapcdn.com/font-awesome/3.0.2/css/font-awesome.css" rel="stylesheet">
+<link rel="stylesheet" href="/stylecf/jquery.fancybox.css" type="text/css" media="screen" />
+
 <!--<link rel="stylesheet" href="/stylecf/supersized.css" type="text/css" media="screen" />-->
 
 <style>
-
-.d-tj-black-box-container .d-tj-black-box
-{
-  min-height:200px;
-}
-
 </style>
+
+<!--venue modal-->
+<? $tourDetail = (json_decode($campaign));
+  foreach($tourDetail as $tourDetail) 
+  { 
+    $venuesDetail = $tourDetail->venues; 
+  } 
+  
+  foreach($venuesDetail as $venue)
+  { 
+    $venue_name = $venue->venue_name;
+    $venue_id = $venue->venue_id;
+    $city = $venue->city;
+    $image = $venue->image;
+    $desc = $venue->desc;
+    $link = $venue->link;
+    $contact = $venue->contact;
+  ?>
+    <html>
+    <body>
+    <div class="venue-form<? print($venue_id); ?>" style="display: none;" >
+      <div class="modal-content socialModal">
+        <div class="modal-header">
+          <h4><? print($venue_name); ?></h4>
+        </div>
+        <div class="modal-body modal-link">
+
+          <div class="row">
+            <div class="col-md-12">
+                  <img src="/img/temp/<? print($image); ?>" style="margin-right:10px" align="left" alt="" height="150" width="150">
+                  <h4><? print($desc); ?></h4>  
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <h4>Location: <? print($city); ?></h4>
+              <h4>Contact: <? print($contact); ?></h4>
+              <a target="_blank" href="http://<? print($link);?>"><h4>Link</h4></a> 
+            </div> 
+          </div>
+
+      </div>
+        <div class="modal-footer">
+          <a href="javascript:;" onclick="$.fancybox.close();" class="btn blk-btn" data-dismiss="modal">Close</a> 
+        </div>
+      </div>
+    </div>
+  </body>
+  </html>
+<? } ?>
+<!--/venue modal-->
 
 </head>
 <body>
@@ -27,34 +74,52 @@
     { 
       $artist_name = $row->artist_name;
       $pledges = $row->pledges;
+      $venues = $row->venues;
+      $tour_name = $row->$tour_name;
     } 
 ?>
 <div class="d-tj-bg-overlay">
   	<div class="container d-tj-container"> <a title="Revolutionizing Live Entertainment" href="http://www.tommyjams.com/" class="d-tj-logo"><img src="/img/tj.jpg" height="64" alt=""/></a>
   	<div class="d-tj-box " >
       <div class="row d-tj-tour">
-        <div class="col-sm-12 col-xs-12 col-md-4"> 
-        	<img src="/img/temp/31.jpg" alt="" style="min-width:250px;min-height:200px">
-        </div>
-        <div class="col-sm-12 col-md-8 d-tj-black-box-container" >
-          <div class="d-tj-black-box d-tj-tour-right" > 
-            <h3 class="raise">Contribution Summary</h3> 
-              <div class="col-sm-12 col-md-3 " >
-                <h4 class="tgt" >Ticket Type</h4>
-  	        		<h4 class="tgt" >Selected Perk</h4>
-  	        		<h4 class="tgt" >Contribution</h4>
-	            </div>
-              <div class="col-sm-12 col-md-9 " >
-	           		<h4 class="tgt" >Silver</h4>
-	        		  <h4 class="tgt" >Entry to both gigs + Band artwork + Digital download of Gig & Studio recordings</h4>
-	        		  <h4 class="tgt" >&#8377 400</h4>
-              </div>	
+        <div class="col-sm-12 col-xs-12 col-md-6">
+          <div class="row" style="margin:0;">
+              <h3><? print($tour_name); ?></h3>
           </div>
+          <div class="row text-center">
+            <? foreach($venues as $venue){ ?>
+            <?
+              $venue_name = $venue->venue_name;
+              $venue_id = $venue->venue_id;
+              $city = $venue->city;
+              $image = $venue->image;
+            ?>
+            <div class="col-md-6 col-sm-6  d-tj-tour-left" >  
+            <div style="background:black">
+              <a href="javascript:;" onclick="venueBox(<? print($venue_id); ?>);" data-toggle="modal" >
+                <img src="img/temp/<? print($image); ?>" alt="">
+              </a>
+            </div>
+              <h4>
+                <span ><? print($venue_name); ?></span> <? print($city); ?>
+              </h4> 
+            </div>
+            <? 
+              } 
+            ?>
+          </div>   
+        </div>
+
+        <div class="col-sm-12 col-xs-12 col-md-6"> 
+        	<input class="form-control input-lg pull-left" type="text" id="phone" name="phone" placeholder="ENTER NAME"></input>
+          <input class="form-control input-lg pull-left" type="text" id="phone" name="phone" placeholder="ENTER EMAIL"></input>
+          <input class="form-control input-lg pull-left" type="text" id="phone" name="phone" placeholder="ENTER PHONE NUMBER [10-DIGIT]"></input>
+          <input class="form-control input-lg pull-left" type="text" id="phone" name="phone" placeholder="ENTER LOCATION"></input>
         </div>
       </div>
     </div>
 
-    <div class="col-sm-12 col-md-7 d-tj-black-box d-tj-offset-top-30" >
+    <div class="col-sm-12 col-md-12 d-tj-black-box d-tj-offset-top-30" >
       <div class="row d-tj-tour">
         <div class="d-tj-pledge">
           <? foreach($pledges as $pledge)
@@ -62,25 +127,31 @@
               $amount = $pledge->amount;
               $pledge_desc = $pledge->desc;
           ?>
-          <div class="col-sm-12 col-md-12 d-tj-box d-tj-offset-top-20" >
-            <div style=""><? print($amount); ?></div>
-            <div style=""><? print($pledge_desc); ?></div>
+          <div class="col-sm-12 col-md-8 d-tj-box d-tj-offset-top-20" >
+            <div style="color:black;">Silver</div>
+            <div style="color:black;">&#8377 <? print($amount); ?></div>
+            <div style="color:black;"><? print($pledge_desc); ?></div>
             <!--<div class="seperator" ></div>-->
-          </div>  
+          </div> 
+          <div class="col-sm-12 col-md-4 d-tj-black-box d-tj-offset-top-20" >
+            <div style="">2</div>
+          </div> 
           <? } ?>
+        </div>
+
+        <div class="text-center" >
+          <input style="margin-top:10px" class="apply-btn" onclick="window.open('/roadshows', '_blank');" type="button" value="PAY">
+          <input style="margin-top:10px" class="apply-btn" onclick="window.open('/tours', '_blank');" type="button" value="GO BACK [EDIT]">
         </div>
       </div>
     </div>
 
-    <div class="col-sm-12 col-md-5 d-tj-black-box d-tj-offset-top-30" >
-      <div class="col-sm-12 col-md-4 d-tj-black-box">
-
+    <div class="col-sm-12 col-md-12 d-tj-box d-tj-offset-top-30" >
+      <div class="col-sm-12 col-md-6 d-tj-black-box">
+        <div style="">Social Share</div>
       </div>
-      <div class="col-sm-12 col-md-12 d-tj-box d-tj-tour-right-edit" >
-        <input class="form-control input-lg pull-left" type="text" id="phone" name="phone" placeholder="ENTER NAME"></input>
-        <input class="form-control input-lg pull-left" type="text" id="phone" name="phone" placeholder="ENTER EMAIL"></input>
-        <input class="form-control input-lg pull-left" type="text" id="phone" name="phone" placeholder="ENTER PHONE NUMBER [10-DIGIT]"></input>
-        <input type="submit" value="PROCEED" >
+      <div class="col-sm-12 col-md-6 d-tj-black-box d-tj-tour-right-edit" >
+        <div style="">Fun Fact</div>
       </div> 
     </div>
 
@@ -94,8 +165,30 @@
 
 </div>   
 
+<script>
+
+function venueBox(id)
+  {
+    var a = 'venue-form' + id;
+
+    $.fancybox(
+        $('.'+a).html(),
+        {
+            'width'             : 950,
+            'height'            : 1100,
+            'autoScale'         : false,
+            'transitionIn'      : 'none',
+            'transitionOut'     : 'none',
+            'hideOnContentClick': false,
+         }
+    );  
+  } 
+
+</script>
+
 <script src="/script/jquery.js"></script> 
 <script src="/script/bootstrap.min.js"></script>
+<script type="text/javascript" src="/script/jquery.fancybox.js"></script>
 </body>
 </html>  
     
