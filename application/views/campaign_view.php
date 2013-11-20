da0badd56c11ad8988731c132fb29d7452e67679
TommyJams <tommyjams.bizspark@outlook.com>
2013-11-20 15:17:49 +0530

    camp view change 5

--- application/views/campaign_view.php
+++ application/views/campaign_view.php
@@ -305,9 +305,9 @@
                   <div style="">
                     <h4 style="font-size:25px">
                       <? print($ticket_type); ?> : &#8377 <? print($amount); ?>
-                      <div onclick="ticketCount('<? print($ticket_type); ?>',plus);" class="btn-plus-count pull-right" style="margin-top:2px"><a class="btn-plus-count-hover"></a></div>
+                      <div onclick="ticketCount('<? print($ticket_type); ?>','plus');" class="btn-plus-count pull-right" style="margin-top:2px"><a class="btn-plus-count-hover"></a></div>
                       <input class="text-center pull-right" type="text" id="<? print($ticket_type); ?>count" name="<? print($ticket_type); ?>count" placeholder="0" value="" disabled="disabled" style="width:25px;height:25px;margin-left:5px;margin-right:5px;border:1px dashed #000000;font-size:20px;color:white;" />
-                      <div onclick="ticketCount('<? print($ticket_type); ?>',minus);" class="btn-minus-count pull-right" style="margin-top:2px"><a class="btn-minus-count-hover"></a></div> 
+                      <div onclick="ticketCount('<? print($ticket_type); ?>','minus');" class="btn-minus-count pull-right" style="margin-top:2px"><a class="btn-minus-count-hover"></a></div> 
                     </h4>
                     <h5><? print($pledge_desc); ?> </h5>
                   </div>
