5dec3b9687e6d8bbf3f0531d8d96a18ceb7cb4c1
TommyJams <tommyjams.bizspark@outlook.com>
2013-11-21 20:59:15 +0530

    camp view change 5

--- application/views/campaign_view.php
+++ application/views/campaign_view.php
@@ -313,8 +313,9 @@
                     <h5 style="width:200px" >
                       <? print($pledge_desc); ?>
                     </h5>
-                    <h5>   
-                    &#8377 <input class="text-center pull-right" type="text" id="<? print($ticket_type); ?>total" name="<? print($ticket_type); ?>total" value="" disabled="disabled" style="border:1px solid #000;font-size:25px;color:white;margin-top:-40px;width:80px;font-family:'Dosis',sans-serif" />
+                    <h5 style="font-size:25px !important;font-family:'Dosis',sans-serif">&#8377 </h5>
+                    <h5> 
+                      <input class="text-center pull-right" type="text" id="<? print($ticket_type); ?>total" name="<? print($ticket_type); ?>total" value="" disabled="disabled" style="border:1px solid #000;font-size:25px;color:white;margin-top:-40px;width:80px;font-family:'Dosis',sans-serif" />
                     </h5>   
                   </div>
                   <div class="seperator" ></div>
@@ -323,7 +324,8 @@
                   ?>
                   <h5 style="font-size:25px !important;font-family:'Dosis',sans-serif"> Grand Total </h5>  
                   <div>  
-                    &#8377 <input class="text-center pull-right" type="text" id="grandTotal" name="grandTotal" value="" disabled="disabled" style="border:1px solid #000;font-size:25px;color:white;width:80px;margin-top: -5px;" />
+                    <h4 style="font-size:25px !important;font-family:'Dosis',sans-serif">&#8377 </h4> 
+                    <input class="text-center pull-right" type="text" id="grandTotal" name="grandTotal" value="" disabled="disabled" style="border:1px solid #000;font-size:25px;color:white;width:80px;margin-top: -5px;" />
                   </div>
                   <? if(isset($ticket_widget)) { ?>
                     <iframe frameborder="0" src="<? print($ticket_widget); ?>" width="315" height="<? print($widget_height); ?>"></iframe>
