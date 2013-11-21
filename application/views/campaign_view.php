9f27d2e7f5589242588a54bf15cfb9cf9d6e9e1a
TommyJams <tommyjams.bizspark@outlook.com>
2013-11-21 20:29:36 +0530

    camp view change 2

--- application/views/campaign_view.php
+++ application/views/campaign_view.php
@@ -321,10 +321,9 @@
                   <? 
                     } }
                   ?>
-                  <div style="font-family:'Dosis',sans-serif" >
-                    <h5 style="font-size:25px">Grand Total</h5>  
-                    <input class="text-center pull-right" type="text" id="grandTotal" name="grandTotal" value="" disabled="disabled" style="border:1px solid #000;font-size:25px;color:white;width:80px;margin-top: -5px;" />
-                  </div>
+                  <h5 style="font-size:25px;font-family:'Dosis',sans-serif">Grand Total</h5>  
+                  <input class="text-center pull-right" type="text" id="grandTotal" name="grandTotal" value="" disabled="disabled" style="border:1px solid #000;font-size:25px;color:white;width:80px;margin-top: -5px;" />
+  
                   <? if(isset($ticket_widget)) { ?>
                     <iframe frameborder="0" src="<? print($ticket_widget); ?>" width="315" height="<? print($widget_height); ?>"></iframe>
                     <div class="d-tj-overlay" style="width:105%; z-index:1000; position:relative; background-color:black;"> 
