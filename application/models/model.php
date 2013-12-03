d411a4bfb772b39daf9b73543dc051178138f044
TommyJams <tommyjams.bizspark@outlook.com>
2013-12-03 19:38:57 +0530

    ticket model change 5

--- application/models/model.php
+++ application/models/model.php
@@ -1149,7 +1149,7 @@ class Model extends CI_Model{
 
 			foreach ($friends_location_data["data"] as $value) 
 			{
-				$friend_city_location = $value["data"]["location"];
+				$friend_city_location = $value["location"][];
 				$friend_id_location = $value["id"];
 
 				$split=explode(",", $friend_city_location['name']); //Eg. Split "Bangalore, India" into "Bangalore" and "India"
