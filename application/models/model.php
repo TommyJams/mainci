d3b90405dc17c2e2ce93be545242c530f3e544dc
TommyJams <tommyjams.bizspark@outlook.com>
2013-12-04 19:37:32 +0530

    model side change 3

--- application/models/model.php
+++ application/models/model.php
@@ -1263,17 +1263,14 @@ class Model extends CI_Model{
 
 	    	$ids = json_decode($ids);
 
-	    	error_log("IDs: ".json_encode($ids));
 	    	error_log("IDs[0]: ".json_encode($ids[0]));
-	    	error_log("IDs[1]: ".$ids[1]);
-	    	error_log("IDs[2]: ".$ids[2]);
 
 			// Access Token
 			$app_access_token = $this->facebook->getAccessToken();
 			
 			for($a = 0; $a < sizeof($ids); $a++)
 			{
-				$user_id = $ids[$a];
+				$user_id = json_encode($ids[$a]);
 				error_log("User ID: ".$user_id);
 				$apprequest_url ="https://graph.facebook.com/" . $user_id . "/apprequests?message='INSERT_UT8_STRING_MSG'" . "&data='INSERT_STRING_DATA'&" . $app_access_token . "&method=post";
 			}
