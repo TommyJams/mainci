e482789638c765813a21bc676ba7661f41dc2750
TommyJams <tommyjams.bizspark@outlook.com>
2013-12-04 20:10:02 +0530

    model change 11

--- application/models/model.php
+++ application/models/model.php
@@ -1269,7 +1269,7 @@ class Model extends CI_Model{
 			for($a = 0; $a < sizeof($ids); $a++)
 			{
 				$user_id = json_encode($ids[$a]);
-				error_log("User ID: ".$user_id);
+				error_log("User ID: ".json_decode($user_id));
 				$apprequest_url ="https://graph.facebook.com/" . $user_id . "/apprequests?message='INSERT_UT8_STRING_MSG'" . "&data='INSERT_STRING_DATA'&" . $app_access_token . "&method=post";
 				
 				$result = file_get_contents($apprequest_url);
