585671c282b551e131719f5b9c9c9f3571538716
TommyJams <tommyjams.bizspark@outlook.com>
2013-12-04 16:40:20 +0530

    ticket view change 11

--- application/models/model.php
+++ application/models/model.php
@@ -1266,7 +1266,7 @@ class Model extends CI_Model{
 			for($a = 0; $a < sizeof($ids); $a++)
 			{
 				$user_id = $ids[$a];
-				error_log("User ID: ".$user_id)
+				error_log("User ID: ".$user_id);
 				$apprequest_url ="https://graph.facebook.com/" . $user_id . "/apprequests?message='INSERT_UT8_STRING_MSG'" . "&data='INSERT_STRING_DATA'&" . $app_access_token . "&method=post";
 			}
 
