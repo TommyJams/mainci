f152a4d0794d02e33050beec84ff10f49d7e3c8e
TommyJams <tommyjams.bizspark@outlook.com>
2013-12-04 16:17:52 +0530

    ticket view change 5

--- application/views/ticketpage_view.php
+++ application/views/ticketpage_view.php
@@ -75,7 +75,7 @@
               				}
               			}
           			?>
-          			<div class="text-center" onclick="sendRequest('<? print($ids); ?>')" >
+          			<div class="text-center d-tj-offset-top-30" onclick="sendRequest('<? print(json_decode($ids)); ?>')" >
               			<input style="" type="button" value="INVITE">
             		</div>
 	            </div>
