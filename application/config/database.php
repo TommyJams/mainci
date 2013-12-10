32eee003970e3fbb37c9a7fec2fd5be01664ee75
TommyJams <nikhilk218@gmail.com>
2013-12-10 16:48:40 +0530

    Revert "database change 3"
    This reverts commit eedd4f046957c1d717a68a6bd1fde85eaf471d49.

--- application/config/database.php
+++ application/config/database.php
@@ -52,13 +52,13 @@
 */
 
 $active_group = 'default';
-$active_record = FALSE;
+$active_record = TRUE;
 
 $db['default']['hostname'] = '';
 $db['default']['username'] = '';
 $db['default']['password'] = '';
 $db['default']['database'] = '';
-$db['default']['dbdriver'] = 'mysql';
+$db['default']['dbdriver'] = '';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
