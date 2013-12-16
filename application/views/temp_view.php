7e59ebd969f4c8d20162b8e3877cd8b6c3f44b89
TommyJams <nikhilk218@gmail.com>
2013-12-16 16:19:20 +0530

    SLP Issue 13

--- application/views/temp_view.php
+++ application/views/temp_view.php
@@ -10,8 +10,8 @@
 <link href="/stylecf/bootstrap.min.css" rel="stylesheet" media="screen">
 <!--<link href="/stylecf/tj.css" rel="stylesheet" media="screen">-->
 <link rel="stylesheet" href="/style/supersized/supersized.css" type="text/css" media="screen" />
-<!--<link rel="stylesheet" type="text/css" href="/style/jquery.qtip.css"/>
-<link rel="stylesheet" href="/stylecf/jquery.fancybox.css" type="text/css" media="screen" />-->
+<link rel="stylesheet" type="text/css" href="/style/jquery.qtip.css"/>
+<!--<link rel="stylesheet" href="/stylecf/jquery.fancybox.css" type="text/css" media="screen" />-->
 <script type="text/javascript" src="<?php echo base_url();?>script/linkify.js"></script>
 <script type="text/javascript" src="<?php echo base_url();?>script/script.js"></script>
 
