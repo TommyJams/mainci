522514bfd604dcefb9ae35d6d724dfa1ed88f7a2
TommyJams <nikhilk218@gmail.com>
2013-12-16 16:11:44 +0530

    SLP Issue 11

--- application/views/temp_view.php
+++ application/views/temp_view.php
@@ -7,11 +7,11 @@
 <link href="/favicon.ico" rel="shortcut icon" type="image/x-icon">
 <!-- Bootstrap -->
 <link href='http://fonts.googleapis.com/css?family=Dosis' rel='stylesheet' type='text/css'>
-<!--<link href="/stylecf/bootstrap.min.css" rel="stylesheet" media="screen">-->
-<!--<link href="/stylecf/tj.css" rel="stylesheet" media="screen">-->
+<link href="/stylecf/bootstrap.min.css" rel="stylesheet" media="screen">
+<link href="/stylecf/tj.css" rel="stylesheet" media="screen">
 <link rel="stylesheet" href="/stylecf/supersized.css" type="text/css" media="screen" />
-<!--<link rel="stylesheet" type="text/css" href="/style/jquery.qtip.css"/>
-<link rel="stylesheet" href="/stylecf/jquery.fancybox.css" type="text/css" media="screen" />-->
+<link rel="stylesheet" type="text/css" href="/style/jquery.qtip.css"/>
+<link rel="stylesheet" href="/stylecf/jquery.fancybox.css" type="text/css" media="screen" />
 <script type="text/javascript" src="<?php echo base_url();?>script/linkify.js"></script>
 <script type="text/javascript" src="<?php echo base_url();?>script/script.js"></script>
 
