da0032b5bd1f4df99c42b3a21a361a801b0039e6
TommyJams <tommyjams.bizspark@outlook.com>
2014-01-25 14:47:38 +0530

    event page update 25th Jan.

--- application/views/landingpage_view.php
+++ application/views/landingpage_view.php
@@ -255,6 +255,22 @@
               <div class="event-ticker">
                 <ul>
                   <li>
+                  <a href="http://tommyjams.com/blog/events/event/baiju-dharmajan-syndicate-phoenix-bangalore/" target="_blank" class="" style="width: 100%">  
+                    <div class="col-md-12 col-sm-12 col-xs-12 d-tj-offset-top-10 ">
+                      <div class="col-md-2 col-xs-3 col-sm-2 d-tj-p0"> <img src="/image/eventTiles/phoenixmall.jpg" alt=""></div>
+                      <div class="col-md-10 col-xs-9 col-sm-10 d-tj-p0">
+                        <div class="col-md-9 col-sm-9 col-xs-12 d-tj-p0" >
+                          <h5>Baiju Dharmajan Syndicate @ Phoenix Marketcity</h5>
+                        </div>
+                        <div class="col-md-3 col-sm-3 col-xs-12 event-date" >
+                          <h5>26-Jan</h5>
+                        </div>
+                      </div>
+                      <div class="clearfix"></div>
+                    </div>
+                  </a>  
+                  </li>
+                  <li>
                   <a href="http://tommyjams.com/blog/events/event/alaap-phoenix-bangalore/" target="_blank" class="" style="width: 100%">  
                     <div class="col-md-12 col-sm-12 col-xs-12 d-tj-offset-top-10 ">
                       <div class="col-md-2 col-xs-3 col-sm-2 d-tj-p0"> <img src="/image/eventTiles/phoenixmall.jpg" alt=""></div>
@@ -301,22 +317,6 @@
                       <div class="clearfix"></div>
                     </div>
                   </a>
-                  </li>
-                  <li>
-                  <a href="http://tommyjams.com/blog/events/event/white-sugar-phoenix-bangalore/" target="_blank" class="" style="width: 100%">  
-                    <div class="col-md-12 col-sm-12 col-xs-12 d-tj-offset-top-10 ">
-                      <div class="col-md-2 col-xs-3 col-sm-2 d-tj-p0"> <img src="/image/eventTiles/phoenixmall.jpg" alt=""></div>
-                      <div class="col-md-10 col-xs-9 col-sm-10 d-tj-p0">
-                        <div class="col-md-9 col-sm-9 col-xs-12 d-tj-p0" >
-                          <h5>White Sugar @ Phoenix Marketcity</h5>
-                        </div>
-                        <div class="col-md-3 col-sm-3 col-xs-12 event-date" >
-                          <h5>10-Jan</h5>
-                        </div>
-                      </div>
-                      <div class="clearfix"></div>
-                    </div>
-                  </a>  
                   </li>
                 </ul>
               </div>
