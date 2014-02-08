6a6e249910c111784177d5a291d7abd793a6d6ea
TommyJams <tommyjams.bizspark@outlook.com>
2014-02-08 19:11:02 +0530

    event update 8th Feb

--- application/views/landingpage_view.php
+++ application/views/landingpage_view.php
@@ -255,6 +255,22 @@
               <div class="event-ticker">
                 <ul>
                   <li>
+                  <a href="http://tommyjams.com/blog/events/event/mrittika-phoenix-bangalore/" target="_blank" class="" style="width: 100%">  
+                    <div class="col-md-12 col-sm-12 col-xs-12 d-tj-offset-top-10 ">
+                      <div class="col-md-2 col-xs-3 col-sm-2 d-tj-p0"> <img src="/image/eventTiles/phoenixmall.jpg" alt=""></div>
+                      <div class="col-md-10 col-xs-9 col-sm-10 d-tj-p0">
+                        <div class="col-md-9 col-sm-9 col-xs-12 d-tj-p0" >
+                          <h5>Mrittika @ Phoenix Marketcity</h5>
+                        </div>
+                        <div class="col-md-3 col-sm-3 col-xs-12 event-date" >
+                          <h5>9-Feb</h5>
+                        </div>
+                      </div>
+                      <div class="clearfix"></div>
+                    </div>
+                  </a>  
+                  </li>
+                  <li>
                   <a href="http://tommyjams.com/blog/events/event/perfect-strangers-phoenix-bangalore/" target="_blank" class="" style="width: 100%">
                     <div class="col-md-12 col-sm-12 col-xs-12 d-tj-offset-top-10 ">
                       <div class="col-md-2 col-xs-3 col-sm-2 d-tj-p0"> <img src="/image/eventTiles/phoenixmall.jpg" alt=""></div>
@@ -296,22 +312,6 @@
                         </div>
                         <div class="col-md-3 col-sm-3 col-xs-12 event-date" >
                           <h5>26-Jan</h5>
-                        </div>
-                      </div>
-                      <div class="clearfix"></div>
-                    </div>
-                  </a>  
-                  </li>
-                  <li>
-                  <a href="http://tommyjams.com/blog/events/event/alaap-phoenix-bangalore/" target="_blank" class="" style="width: 100%">  
-                    <div class="col-md-12 col-sm-12 col-xs-12 d-tj-offset-top-10 ">
-                      <div class="col-md-2 col-xs-3 col-sm-2 d-tj-p0"> <img src="/image/eventTiles/phoenixmall.jpg" alt=""></div>
-                      <div class="col-md-10 col-xs-9 col-sm-10 d-tj-p0">
-                        <div class="col-md-9 col-sm-9 col-xs-12 d-tj-p0" >
-                          <h5>Alaap @ Phoenix Marketcity</h5>
-                        </div>
-                        <div class="col-md-3 col-sm-3 col-xs-12 event-date" >
-                          <h5>19-Jan</h5>
                         </div>
                       </div>
                       <div class="clearfix"></div>
