a9790be3ec2490b5b1fecb6e0a93d0594b78b50a
TommyJams <nikhilk218@gmail.com>
2014-03-13 18:15:44 -0300

    Language Changes: Landing Page 6

--- application/language/espanol/strings_lang.php
+++ application/language/espanol/strings_lang.php
@@ -8,8 +8,8 @@
     $lang['str_title_roadshows'] = "TOMMYJAMS ROADSHOWS";
     $lang['str_desc_roadshows'] = "Las campañas de RoadShows&#39 son iniciados por los artistas para los tours que quieren aventurarse en. Cada vez que usted compra un boleto, un artista lleva un paso más cerca de su viaje de ensueño a través de varias ciudades del país. No sólo eso, se obtiene acceso exclusivo a todos los espectáculos que forman parte de esa gira, e incluso obtener regalos extra de la banda en el lugar de días para que el evento realmente especial para usted.";
     $lang['str_book_roadshows'] = "Reserve un billete ahora!";
-    $lang['btn_all_campaigns'] = "TODAS LAS CAMPAÑAS";
-    $lang['btn_start_campaign'] = "EMPIEZA LA CAMPAÑA";
+    $lang['btn_all_campaigns'] = "TODAS CAMPAÑAS";
+    $lang['btn_start_campaign'] = "EMPIEZA CAMPAÑA";
     $lang['str_title_events'] = "CONCIERTOS";
     $lang['str_title_network'] = "CONECTAR CON NOSOTROS";
     $lang['btn_your_email'] = "Su dirección de e-mail";
