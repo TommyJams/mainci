e241cca29b1b2fae0e54a983651a7b7fdef7fd45
TommyJams <nikhilk218@gmail.com>
2014-03-13 18:14:11 -0300

    Language Changes: Landing Page 5

--- application/language/espanol/strings_lang.php
+++ application/language/espanol/strings_lang.php
@@ -6,13 +6,13 @@
     $lang['btn_book_events'] = "EVENTOS DE LIBROS";
     $lang['btn_book_tours'] = "RESERVAR EXCURSIONES";
     $lang['str_title_roadshows'] = "TOMMYJAMS ROADSHOWS";
-    $lang['str_desc_roadshows'] = "Las campañas de RoadShows&#39 son iniciados por los artistas para los tours que quieren aventurarse en. Cada vez que usted compra un boleto, un artista lleva un paso más cerca de su viaje de ensue&#164o a trav&#130s de varias ciudades del pa&#161s. No s&#162lo eso, se obtiene acceso exclusivo a todos los espect&#160culos que forman parte de esa gira, e incluso obtener regalos extra de la banda en el lugar de d&#161as para que el evento realmente especial para usted.";
+    $lang['str_desc_roadshows'] = "Las campañas de RoadShows&#39 son iniciados por los artistas para los tours que quieren aventurarse en. Cada vez que usted compra un boleto, un artista lleva un paso más cerca de su viaje de ensueño a través de varias ciudades del país. No sólo eso, se obtiene acceso exclusivo a todos los espectáculos que forman parte de esa gira, e incluso obtener regalos extra de la banda en el lugar de días para que el evento realmente especial para usted.";
     $lang['str_book_roadshows'] = "Reserve un billete ahora!";
-    $lang['btn_all_campaigns'] = "TODAS LAS CAMPA&#165AS";
-    $lang['btn_start_campaign'] = "EMPIEZA LA CAMPA&#165A";
+    $lang['btn_all_campaigns'] = "TODAS LAS CAMPAÑAS";
+    $lang['btn_start_campaign'] = "EMPIEZA LA CAMPAÑA";
     $lang['str_title_events'] = "CONCIERTOS";
     $lang['str_title_network'] = "CONECTAR CON NOSOTROS";
-    $lang['btn_your_email'] = "Su dirección de correo electr&#162nico";
+    $lang['btn_your_email'] = "Su dirección de e-mail";
     $lang['btn_subscribe'] = "Suscribir";
-    $lang['str_title_seen'] = "SEG&#233N LO VISTO EN";
+    $lang['str_title_seen'] = "SEGÚN LO VISTO EN";
 ?>
