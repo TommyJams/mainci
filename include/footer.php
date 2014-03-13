fd3b50711e3bff05dd1aa84ab8c8c60cd3e3c9e1
TommyJams <nikhilk218@gmail.com>
2014-03-13 11:29:24 -0300

    Language Changes 2
    Adding icons for switching language

--- include/footer.php
+++ include/footer.php
@@ -2,13 +2,17 @@
     <div class="col-md-12 col-sm-12 col-xs-12 footer-main">
       <div class="col-md-3 col-sm-12 col-xs-12 footer-top">
         <div class="col-md-12 f-logo"> <a href="http://www.tommyjams.com/"><img src="/img/tj.jpg" height="50" alt=""> </a> </div>
-        <div class="col-md-12 f-social">
-          <ul class=" list-unstyled social-list clear-fix">
+        <div class="col-md-12 f-lang">
+          <ul class=" list-unstyled lang-list clear-fix">
+            <!--
             <li><a style="margin-right:2px;margin-top:2px;" href="http://www.facebook.com/tommyjams.live" title="" alt="Facebook" target="_blank" class="social-list-facebook" data-original-title="Facebook"></a></li>
             <li><a style="margin-right:2px;margin-top:2px;" href="http://twitter.com/TommyJams" title="" alt="Twitter" target="_blank" class="social-list-twitter" data-original-title="Twitter"></a></li>
             <li style="margin-top:2px;" class="f-like">
               <iframe src="http://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Ftommyjams.live&amp;width=100&amp;height=21&amp;colorscheme=light&amp;layout=button_count&amp;action=like&amp;show_faces=true&amp;send=false&amp;appId=625320827487862" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:21px;" allowTransparency="true"></iframe>
             </li>
+            -->
+            <li><a href="http://tommyjams.com" title="" alt="India" class="lang-list-india" data-original-title="India"></a></li>
+            <li><a href="http://es.tommyjams.com" title="" alt="Chile" class="lang-list-chile" data-original-title="Chile"></a></li>
           </ul>
         </div>
       </div>
