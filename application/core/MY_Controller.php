702b6003b71365d6a1812e74aa515f97a6a911cd
TommyJams <nikhilk218@gmail.com>
2014-03-17 19:20:57 -0300

    Language Change: Promoter 2

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -19,6 +19,12 @@ class MY_Controller extends CI_Controller {
 
 class Base extends MY_Controller{
 
+	public function __construct() {
+
+        parent::__construct();
+
+    }
+
 	public function checkUserSession(){
 
 		$sessionArray = $this->session->all_userdata();
