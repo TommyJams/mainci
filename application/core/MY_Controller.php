a0f95255829344dd3528500cd19e8747ec5d83d6
TommyJams <nikhilk218@gmail.com>
2014-03-19 11:44:12 -0300

    Language Change: Autodetect 15

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -68,6 +68,8 @@ class MY_Controller extends CI_Controller {
 		    $lang = $this->config->item('default_language');
 		}
 
+		error_log('Saving Language: '.$lang);
+
 		// Whatever we decided the lang was, save it for next time to avoid working it out again
 		$this->session->set_userdata('lang_code', $lang);
 
