d2b5914ebc5f4ac551be73a10736c49cacd30fe9
TommyJams <nikhilk218@gmail.com>
2014-03-24 19:06:51 -0300

    Language Change: Profile 9

--- application/views/include/profile.php
+++ application/views/include/profile.php
@@ -186,7 +186,7 @@
                     <p>
 						<? 
 							/*convert to URL*/
-							$aboutStr = preg_replace("/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/",'<a href="$1">$1</a>', $about); 
+							$aboutStr = preg_replace("/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/",'<a href="$1$2" target="_blank">$1$2</a>', $about); 
 
 							/*format newlines*/
 							$aboutStr = nl2br("$aboutStr");
