a7c1fe98485a773a10c7b00747701425ad4a46a5
TommyJams <nikhilk218@gmail.com>
2014-06-03 17:56:27 -0400

    Solstice Landing Page 14

--- application/views/solstice_view.php
+++ application/views/solstice_view.php
@@ -135,6 +135,28 @@
                   </div>
                 </div>
                </li>
+               <li>
+                <div class=" col-md-12" style="padding: 5px;">
+                  <h4 class="d-tj-slide-head" >artist_name</h4>
+                  <div class="d-tj-slide-body " style="">
+                    <div class="d-tj-slide-img" onclick="window.open('');" style="background-image:url(); ?>)">  
+                      <div class="d-tj-slide-overlay-img"> <img src="/image/radioone/icon/image_overlay_grey.png" alt=""/> </div>
+                    </div>
+                    <div>
+                      <div class="col-md-6 d-tj-slide-btm col-sm-6 col-xs-6">
+                        <ul class="list-unstyled  " >
+                          <li>
+                            <p>venue 4</p>
+                          </li>
+                          <li >
+                            <p >time 4</p>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+               </li>
             </ul>
             <div class="clearfix"></div>
             <a id="prev5" class="prev" href="#" ></a> <a id="next5" class="next" href="#"  ></a> </div>
