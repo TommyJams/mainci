45ea83044049a517f6c355b57b0038bc4c765126
TommyJams <nikhilk218@gmail.com>
2015-07-05 18:07:36 +0800

    Revert "Testing Registration 1"
    This reverts commit 57d5f3f10c1820ebf76e5465c464ea12bd4aa453.

--- application/views/registration1_view.php
+++ application/views/registration1_view.php
@@ -11,8 +11,3 @@
 	height="330">
 
 </iframe>
-
-
-<input type="text" id="fbname" name="fbname" style="width:200px;" value=""/>
-
-<a href="<? print(base_url()); ?>fbconnect/registerMethod/fbregistered&fb_register=true&fields=<?php echo $fb_fields;?>">SUBMIT</a>
